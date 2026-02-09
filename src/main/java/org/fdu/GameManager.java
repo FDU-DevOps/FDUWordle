0402d57b75bb0fd68fde0ab319c1a3bb0d475737
abehrensFDU <a.behrens@student.fdu.edu>
2026-02-08 19:30:09 -0500

    DEVOPS-174 Javadoc comments from software architecture.

--- src/main/java/org/fdu/GameManager.java
+++ src/main/java/org/fdu/GameManager.java
@@ -1,5 +1,13 @@
 package org.fdu;
 
+/**
+ * Controls the game flow: <br>
+ * start game <br>
+ * accept guesses (up to 6) to determine WIN / LOSS <br>
+ * Uses WordRepo for dictionary validation/answer selection <br>
+ * Produces feedback results for ConsoleUI to display <br>
+ */
+
 public class GameManager {
     private final ConsoleUI ui;
     private final WordRepo repo;
--- src/test/java/org/fdu/AppTest.java
+++ src/test/java/org/fdu/AppTest.java
@@ -5,6 +5,10 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Basic tests for app wiring and flow (expand later into unit tests for GameManager and WordRepo)
+ */
+
 class AppTest {
     @Test
     void helloAppTest() {
