5c57024c8336aee472d563029004d5fa151a75ed
abehrensFDU <a.behrens@student.fdu.edu>
2026-02-08 19:30:09 -0500

    DEVOPS-174 Javadoc comments from software architecture.

--- src/main/java/org/fdu/GameManager.java
+++ src/main/java/org/fdu/GameManager.java
@@ -1,4 +1,12 @@
 package org.fdu;
 
+/**
+ * Controls the game flow: <br>
+ * start game <br>
+ * accept guesses (up to 6) to determine WIN / LOSS <br>
+ * Uses WordRepo for dictionary validation/answer selection <br>
+ * Produces feedback results for ConsoleUI to display <br>
+ */
+
 public class GameManager {
 }
