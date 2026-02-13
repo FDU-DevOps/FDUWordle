f992258e97237cc594b6d985c0b29c3ae58e634a
lvallone3315 <55007696+lvallone3315@users.noreply.github.com>
2026-02-12 20:22:01 -0500

    DevOps-190 - merged main() javadoc into branch.

--- src/main/java/org/fdu/App.java
+++ src/main/java/org/fdu/App.java
@@ -5,8 +5,7 @@ package org.fdu;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
         WordRepo repo = new WordRepo();
         GameManager manager = new GameManager(repo);
         manager.runGame();
