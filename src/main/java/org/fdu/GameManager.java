04d35030c46d5c561d0dbda58b862032fbd8ab57
Xavier-Orrala <xavierorrala12404@gmail.com>
2026-02-14 17:47:07 -0500

    DEVOPS-192 Updated JavaDoc syntax for better looking JavaDoc

--- src/main/java/org/fdu/GameManager.java
+++ src/main/java/org/fdu/GameManager.java
@@ -18,12 +18,12 @@ public class GameManager {
     /**
      * Checks whether the user guess is correct or not <br>
      <p>
-     Scope: Determining if winning condition is met (word match)
+     Scope: Determining if winning condition is met (word match).
      Assumes word is valid and input has been normalized </p>
-     @param playerGuess - user guess for the word
-     @param targetWord - chosen answer for this wordle game
-     @return - boolean flag to show if guess was correct or not
-     @author - Xavier Orrala
+     @param playerGuess user guess for the word
+     @param targetWord chosen answer for this wordle game
+     @return boolean flag to show if guess was correct or not
+     @author Xavier Orrala
      */
     public static boolean isWinningGuess(String playerGuess, String targetWord)
     {
