25f809161966107f97c7776f79d9feab33948e63
Xavier-Orrala <xavierorrala12404@gmail.com>
2026-02-14 21:38:53 -0500

    DEVOPS-192 Edited GenerateColoredFeedback method. Methods runs through guess to check for green guesses first, then yellow, then gray
    Edited comments in method for clarity
    Moved my commented test cases to WordRepoTest to clean up the method

--- src/main/java/org/fdu/WordRepo.java
+++ src/main/java/org/fdu/WordRepo.java
@@ -19,53 +19,59 @@ public class WordRepo {
      @param playerGuess - user guess for the word
      @param targetWord - chosen answer for this wordle game
      @return - enum array with color-coded feedback
-     @author (developer name)
+     @author Xavier Orrala
      */
-
     public static ConsoleUI.FeedbackType[] GenerateColoredFeedback(String playerGuess, String targetWord)
     {
-        // Array to store color coded feedback results
+        // Array to store color coded feedback results -- CAN BE REFACTORED TO INCLUDE MAX_GUESS CONSTANT?
         ConsoleUI.FeedbackType[] results = new ConsoleUI.FeedbackType[5];
 
-        // separate player guess and target word into character array
+        // Separate playerGuess and targetWord into character arrays
         char[] playerGuessToCharArray = playerGuess.toCharArray();
         char[] targetWordToCharArray = targetWord.toCharArray();
 
-        // Create hashmap to store letter and letter count of targetWord
+        // Create hashmap to store letter(s) and letter count of targetWord <KEY: letter, VALUE: count>
         HashMap<Character,Integer> targetWordLetterCount = new HashMap<>();
-        for(char count: targetWordToCharArray)
+        for(char letter: targetWordToCharArray)
         {
-            // Find the count of each letter from the targetWord, add to the hashmap, default value for each letter is 0
-            targetWordLetterCount.put(count, targetWordLetterCount.getOrDefault(count,0)+1);
+            // Find the count of each letter from the targetWord, then add to the hashmap
+            // Default value for count of each letter is 0
+            targetWordLetterCount.put(letter, targetWordLetterCount.getOrDefault(letter,0)+1);
         }
 
-        // run through guess array, compare to target array, color code
-        // Check for Green, then yellow, then gray
-        //int remainingLetterCount = targetWordLetterCount.getOrDefault()
+        // Run through playerGuessToCharArray and compare to targetWordToCharArray - color code as appropriate - prioritize green, then yellow, then gray
+        // First run to check for letters in the correct location (GREEN)
+        for(int i = 0; i < results.length; i++)
+        {
+            if(playerGuessToCharArray[i] == targetWordToCharArray[i])
+            {
+                // Set correct letter location to GREEN enum + decrease count for letter
+                results[i] = ConsoleUI.FeedbackType.GREEN;
+                targetWordLetterCount.put(playerGuessToCharArray[i], targetWordLetterCount.get(playerGuessToCharArray[i]) - 1);
+            }
+        }
 
-        // give color code to each enum
+        // Run to check for correct letters in the incorrect location (YELLOW), then the rest are (GRAY/RESET)
+        for(int i = 0; i < results.length; i++)
+        {
+            // Skip index if letter was correct (GREEN)
+            if(results[i] == null)
+            {
+                char currentGuessCharacter = playerGuessToCharArray[i];
 
-        // make enum array out of character array
-
-
-
-
-        /*
-            Cases:
-            Letter matches
-            Letter doesn't match
-            Letter is present in word, but not in correct spot
-
-            Double Letter Cases
-            User Guess: 2 letters, Target Word: 1 letter, 1 of the letters is in the Correct Location = GREEN for correct letter, other letter is Gray
-            User Guess: 2 letters, Target Word: 1 letter, letters in incorrect location = First instance of letter in guess is yellow, second instance is gray
-            User Guess: 2 letters, Target Word: 2 letter, incorrect location for both = 2 YELLOWS
-            User Guess: 2 letters, Target Word: 2 letters, 2 correct locations = 2 GREEN
-            User Guess: 2 letters, Taget Word: 2 letters: 1 correct location, 1 incorrect location = GREEN and YELLOW
-            User Guess: 1 letter, Target Word: 2 letters, Correct Location for letter guessed = GREEN for correct letter
-            User Guess: 1 letter, Target Word: 2 letters, user letter guessed is in incorrect Location = YELLOW
-            Prioritization rule( Green, then yellow, then gray
-         */
+                // Get the remaining count of specified letter
+                int remainingTargetWordLetterCount = targetWordLetterCount.getOrDefault(currentGuessCharacter, 0);
+                if(remainingTargetWordLetterCount > 0)
+                {
+                    // Set correct letter, but incorrect location to YELLOW enum + decrease count for letter
+                    results[i] = ConsoleUI.FeedbackType.YELLOW;
+                    targetWordLetterCount.put(currentGuessCharacter, remainingTargetWordLetterCount - 1);
+                }
+                else { // Letter not found (no count for specified letter)
+                    results[i] = ConsoleUI.FeedbackType.RESET;
+                }
+            }
+        }
         return results;
     }
 }
--- src/test/java/org/fdu/WordRepoTest.java
+++ src/test/java/org/fdu/WordRepoTest.java
@@ -10,5 +10,21 @@ class WordRepoTest {
     @Test
     @DisplayName("Test method for color coded feedback method")
     void generateColoredFeedbackTest() {
+         /*
+            Cases:
+            Letter matches
+            Letter doesn't match
+            Letter is present in word, but not in correct spot
+
+            Double Letter Cases
+            User Guess: 2 letters, Target Word: 1 letter, 1 of the letters is in the Correct Location = GREEN for correct letter, other letter is Gray
+            User Guess: 2 letters, Target Word: 1 letter, letters in incorrect location = First instance of letter in guess is yellow, second instance is gray
+            User Guess: 2 letters, Target Word: 2 letter, incorrect location for both = 2 YELLOWS
+            User Guess: 2 letters, Target Word: 2 letters, 2 correct locations = 2 GREEN
+            User Guess: 2 letters, Taget Word: 2 letters: 1 correct location, 1 incorrect location = GREEN and YELLOW
+            User Guess: 1 letter, Target Word: 2 letters, Correct Location for letter guessed = GREEN for correct letter
+            User Guess: 1 letter, Target Word: 2 letters, user letter guessed is in incorrect Location = YELLOW
+            Prioritization rule( Green, then yellow, then gray
+         */
     }
 }
