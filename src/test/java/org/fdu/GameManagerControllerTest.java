2e872490116771706fc9398d2c54cee6e323ce2a
Xavier-Orrala <xavierorrala12404@gmail.com>
2026-03-09 17:37:25 -0400

    DEVOPS-333 Added RestTest test case for starting a game

--- src/test/java/org/fdu/GameManagerControllerTest.java
+++ src/test/java/org/fdu/GameManagerControllerTest.java
@@ -3,9 +3,13 @@ package org.fdu;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureRestTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.client.RestTestClient;
+import org.springframework.test.web.servlet.client.assertj.RestTestClientResponse;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 @AutoConfigureRestTestClient
@@ -19,7 +23,6 @@ class GameManagerControllerTest {
     void setUp()
     {
         gameManager = new GameManager();
-        //controller = new GameManagerController(gameManager);
     }
 
     @Test
@@ -51,5 +54,33 @@ class GameManagerControllerTest {
         gameResponse = gameManager.submitGuess(incorrectGuess);
         assertNotEquals(gameManager.getTargetWord(), incorrectGuess.playerGuess());
         assertFalse(gameResponse.hasWon());
+    }
+    @Test
+    @DisplayName("Test starting a new game via API")
+    void testStartGame(@Autowired RestTestClient restClient) {
+        // Hitting api and gathering response
+        RestTestClient.ResponseSpec spec = restClient.post().uri("/api/FDUWordle/start-game").exchange();
+        RestTestClientResponse response = RestTestClientResponse.from(spec);
+
+       // Check for 200 OK response and if DTO items are contained -- just checking fields
+        assertThat(response).hasStatusOk().bodyText().contains("targetWord").contains("hasWon")
+                .contains("guessesUsed").contains("isValidGuess").contains("feedbackColors");
+
+        // Load up a response of /start-game
+        GameResponse loadedGame = restClient.post()
+                .uri("/api/FDUWordle/start-game")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(GameResponse.class)
+                .returnResult()
+                .getResponseBody();
+
+        // Assert that all the proper initialized pieces of the DTO match -- checking actual response
+        assertThat(loadedGame).isNotNull();
+        assertThat(loadedGame.targetWord()).isNotEmpty();
+        assertThat(loadedGame.hasWon()).isFalse();
+        assertThat(loadedGame.guessesUsed()).isEqualTo(0);
+        assertThat(loadedGame.isValidGuess()).isTrue();
+        assertThat(loadedGame.feedbackColors()).isEmpty();
     }
 }
